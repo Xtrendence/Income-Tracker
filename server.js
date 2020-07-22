a14bbc426cb56da13f848674defebdc9286c1319
Xtrendence <xtrendence@yahoo.com>
2020-07-22 10:06:52 +0100

    Added favicon and boilerplate code.

---
+++ server.js
@@ -0,0 +1,18 @@
+const port = 80;
+
+const express = require("express");
+const app = express();
+const server = app.listen(port);
+
+const fs = require("fs");
+const path = require("path");
+const body_parser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use("/assets", express.static("assets"));
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.json());
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
